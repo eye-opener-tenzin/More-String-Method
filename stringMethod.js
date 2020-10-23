cfbd82f480e298937f36b3094d8fcd3eff8caf03
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-22 20:46:14 -0700

    add html and js file

---
+++ stringMethod.js
@@ -0,0 +1,8 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = 'skateboard' //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let result = word.slice(5, 10).replace('o', 'e');
+let facialHair = result;
+
+console.log(facialHair)
